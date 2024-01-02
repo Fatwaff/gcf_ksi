338fd13b2c3cc8203aa95f11aef0286ebe798a07
Fatwaff <fax.mp4@gmail.com>
2024-01-02 17:25:20 +0700

    Add KSI functions for user registration, login, billboard, and rental.

---
+++ billboard/function.go
@@ -0,0 +1,49 @@
+package gcf
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Fatwaff/be_ksi"
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("KSI", KSI_Billboard)
+}
+
+func KSI_Billboard(w http.ResponseWriter, r *http.Request) {
+	allowedOrigins := []string{"https://ksi-billboard.github.io", "http://127.0.0.1:5500", "http://127.0.0.1:5501"}
+	origin := r.Header.Get("Origin")
+
+	for _, allowedOrigin := range allowedOrigins {
+		if allowedOrigin == origin {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			break
+		}
+	}
+	// w.Header().Set("Access-Control-Allow-Origin", "https://intern-monitoring.github.io")
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	if r.Method == http.MethodPost {
+		fmt.Fprintf(w, be_ksi.TambahBillboardHandler("PASETOPUBLICKEY", "MONGOSTRING", "db_ksi", r))
+		return
+	}
+	if r.Method == http.MethodPut {
+		fmt.Fprintf(w, be_ksi.EditBillboardHandler("PASETOPUBLICKEY", "MONGOSTRING", "db_ksi", r))
+		return
+	}
+	if r.Method == http.MethodDelete {
+		fmt.Fprintf(w, be_ksi.HapusBillboardHandler("PASETOPUBLICKEY", "MONGOSTRING", "db_ksi", r))
+		return
+	}
+	// Set CORS headers for the main request.
+	fmt.Fprintf(w, be_ksi.GetBillboarHandler("MONGOSTRING", "db_ksi", r))
+
+}
---
+++ login/function.go
@@ -0,0 +1,37 @@
+package gcf
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/Fatwaff/be_ksi"
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("KSI", KSI_Login)
+}
+
+func KSI_Login(w http.ResponseWriter, r *http.Request) {
+	allowedOrigins := []string{"https://ksi-billboard.github.io", "http://127.0.0.1:5500", "http://127.0.0.1:5501"}
+	origin := r.Header.Get("Origin")
+
+	for _, allowedOrigin := range allowedOrigins {
+		if allowedOrigin == origin {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			break
+		}
+	}
+	// w.Header().Set("Access-Control-Allow-Origin", "https://intern-monitoring.github.io")
+	// Set CORS headers for the preflight request
+	if r.Method == http.MethodOptions {
+		w.Header().Set("Access-Control-Allow-Methods", "POST")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization,Token")
+		w.Header().Set("Access-Control-Max-Age", "3600")
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+	// Set CORS headers for the main request.
+	fmt.Fprintf(w, be_ksi.LogInHandler("PASETOPRIVATEKEY", "MONGOSTRING", "db_ksi", r))
+
+}
